c96c7461bfd5061c37303d1c6dd0a34bdffda7cf
Gareth Cooper <gareth@garethcooper.com>
2024-03-14 10:02:30 +0000

    removed test code

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,7 +1,6 @@
 import { LovelaceCardConfig, Theme } from 'custom-card-helpers';
 
 export interface GcclockSimpleCardConfig extends LovelaceCardConfig {
-  test?: string;
   show_card_background?: boolean;
   smooth_seconds?: boolean;
   theme?: Theme;
